4e53c8e4f3f8218da9e3e94476e9da2b6e84ec1c
BR05 <21fortnitegod@gmail.com>
2026-01-01 15:30:08 +0000

    BR05TagSystem.lua

--- BR05TagSystem.lua
+++ BR05TagSystem.lua
@@ -53,13 +53,14 @@ local SOS_JOIN_PING_VOLUME = 0.10 -- 10 percent volume
 -- Optional per user intros (text popup, glitchy)
 -- CustomUserIntros[UserId] = { Text = "Hello", SoundId = "rbxassetid://123", TextColor = Color3.fromRGB(255,255,255) }
 local CustomUserIntros = {
-
-	local CustomUserIntros = {
+	
 	[7452991350] = { 
 		Text = "XTCY Has Been Summoned.", 
 		SoundId = "rbxassetid://120403072198402", 
 		TextColor = Color3.fromRGB(200, 0, 0) 
 	},
+
+	
 }
 
 
