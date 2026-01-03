80e1e1b0c4fafd3932045b676e3d275e239800e4
BR05 <21fortnitegod@gmail.com>
2026-01-03 18:27:08 +0000

    .lua

--- .lua
+++ .lua
@@ -1,7 +1,14 @@
+ local loaders = {
+    function()
+        loadstring(game:HttpGet(""))()
+    end,
+        wait(0.5)
+    function()
         loadstring(game:HttpGet("https://raw.githubusercontent.com/BR05Lua/SOS/refs/heads/main/SOSMenu.lua"))()
     end,
+        wait(0.5)
     function()
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/BR05Lua/SOS/refs/heads/main/BR05TagSystem.lua"))()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/BR05Lua/SOS/refs/heads/main/Security/BR05"))()
     end
 }
 
@@ -12,5 +19,4 @@ for i, loader in ipairs(loaders) do
         break
     end
 end
-
 print("Thanks To Co Owner For Making The New Loadstring System")
